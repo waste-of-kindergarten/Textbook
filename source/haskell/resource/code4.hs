fa4a3f7e76d5b9f1c91ac5132b73f27a2a17887c
waste-of-kindergarten <2020141461068@stu.scu.edu.cn>
2024-04-15 08:13:15 +0000

    new

--- source/haskell/resource/code4.hs
+++ source/haskell/resource/code4.hs
@@ -5,12 +5,12 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 import Data.Dynamic
 import Data.Typeable (cast)
@@ -36,17 +36,17 @@ data Figure'' =
 
 type Height = Double
 
-data SolidFigure = 
+data SolidFigure =
     Sphere {getDiameter :: Diameter}
     | Cuboid {getLength :: Length, getWidth :: Width, getHeight :: Height}
     | Cylinder {getDiameter :: Diameter, getHeight :: Height}
   deriving (Show)
 
-class Judgeable a where 
+class Judgeable a where
     judgeType :: a -> String
     tellShape :: a -> String
 
-instance Judgeable Figure'' where  
+instance Judgeable Figure'' where
     judgeType :: Figure'' -> String
     judgeType _ = "A Figure"
     tellShape :: Figure'' -> String
@@ -54,16 +54,16 @@ instance Judgeable Figure'' where
     tellShape (Rectangle'' {}) = "A Rectangle"
     tellShape (Triangle'' {}) = "A Triangle"
 
-instance Judgeable SolidFigure where 
+instance Judgeable SolidFigure where
     judgeType :: SolidFigure -> String
     judgeType _ = "A SolidFigure"
     tellShape :: SolidFigure -> String
     tellShape (Sphere _) = "A Sphere"
     tellShape (Cuboid {}) = "A Cuboid"
-    tellShape (Cylinder {}) = "A Cylinder" 
+    tellShape (Cylinder {}) = "A Cylinder"
 
 
-class Judgeable a => Dimension a where 
+class Judgeable a => Dimension a where
   dim :: a -> Int
 
 instance Dimension Figure'' where
@@ -72,15 +72,15 @@ instance Dimension Figure'' where
 instance Dimension SolidFigure where
   dim = const 3
 
-class Judgeable' a where 
-  judgeType' :: a -> String 
+class Judgeable' a where
+  judgeType' :: a -> String
   judgeType' x = "A Default Judgeable Type"
-  tellShape' :: a -> String 
+  tellShape' :: a -> String
   tellShape' x = "A Default Judgeable Shape"
 
 instance Judgeable' SolidFigure
 
-data SolidFigure' = 
+data SolidFigure' =
   Sphere' {getDiameter :: Diameter}
     | Cuboid' {getLength :: Length, getWidth :: Width, getHeight :: Height}
     | Cylinder' {getDiameter :: Diameter, getHeight :: Height}
@@ -88,63 +88,66 @@ data SolidFigure' =
 
 newtype Fig = Fig SolidFigure deriving newtype (Judgeable)
 
-type SynFig = Fig 
+type SynFig = Fig
 
-instance Judgeable' SynFig 
+instance Judgeable' SynFig
 
 deriving instance Eq Figure''
 deriving instance Eq SolidFigure
 deriving instance Eq SolidFigure'
 
 class Same a b where
-  same :: a -> b -> Bool 
+  same :: a -> b -> Bool
   same _ _ = False
 
-instance Same Figure'' SolidFigure  
+instance Same Figure'' SolidFigure
 
-instance Same SolidFigure Figure'' 
+instance Same SolidFigure Figure''
 
-instance Same SolidFigure SolidFigure where 
-  same = (==) 
-
-instance Same Figure'' Figure'' where 
+instance Same SolidFigure SolidFigure where
   same = (==)
 
-class DefaultVal where 
+instance Same Figure'' Figure'' where
+  same = (==)
+
+class DefaultVal where
   unit :: Double
   unit = 1
-  info :: String 
+  info :: String
   info = "global information"
   needinstantiation :: ()
 
 instance DefaultVal where
   needinstantiation = ()
 
--- ordinary instances
-instance Eq a => Same (Maybe a) (Maybe a) where 
-  same Nothing Nothing = True 
-  same (Just a) (Just b) = a == b 
-  same _ _ = False 
 
--- flexible instances
-instance Same [Char] [Char] where 
-  same [] [] = True 
-  same (x:xs) (y:ys) = x == y && same xs ys 
-  same _ _ = False
+class Empty a where
+  isEmpty :: a -> Bool
 
-class WithoutDep a b c where 
-  func :: a -> b -> c 
+-- ordinary instance 
+instance Empty (Maybe a) where
+  isEmpty Nothing = True
+  isEmpty (Just _) = False
 
-instance WithoutDep Int Int Int where 
+
+-- flexible instance
+instance Empty [Char] where
+  isEmpty [] = True
+  isEmpty (x:xs) = False
+
+class WithoutDep a b c where
+  func :: a -> b -> c
+
+instance WithoutDep Int Int Int where
   func = \ x y -> 0
 
 instance WithoutDep Int Int Double where
   func = \ x y -> 0
 
-class WithDep a b c | a b -> c where 
+class WithDep a b c | a b -> c where
   func' :: a -> b -> c
 
-instance WithDep Int Int Int where 
+instance WithDep Int Int Int where
   func' = \ x y -> 0
 
 {-
@@ -154,11 +157,11 @@ instance WithDep Int Int Double where
 -}
 
 class Assoc a b where
-  type FuncType a b :: * 
+  type FuncType a b :: *
   assocfunc :: a -> b -> FuncType a b
 
-instance Assoc Int Int where 
-  type FuncType Int Int = Int 
+instance Assoc Int Int where
+  type FuncType Int Int = Int
   assocfunc a b = 0
 
 -- code4.hs
